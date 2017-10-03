39ba14c8495b51e49c2cbb8919e5257e33900380
Rajasree <me.shree@gmail.com>
2017-10-02 21:11:52 -0700

    Adding Bag

--- src/Main/jithu/abstractjava/arrays/Bag.java
+++ src/Main/jithu/abstractjava/arrays/Bag.java
@@ -2,34 +2,33 @@ package jithu.abstractjava.arrays;
 
 import jithu.abstractjava.common.IBag;
 
+import java.util.Arrays;
+
 /**
  * Created by aj on 10/1/17.
  */
 public class Bag<T> implements IBag<T> {
-
-    private static final int MAX_SIZE = 10;
-    private String i;
-    private T[] mBag;
+    private T[] mElements;
     private int mCurrentElementIndex;
 
-    public Bag() {
-        mBag = (T[]) new Object[MAX_SIZE];
+    public Bag(T[] elements) {
+        mElements = elements;
     }
 
     @Override
     public int getCurrentSize() {
-        return mBag.length;
+        return mCurrentElementIndex;
     }
 
     @Override
     public boolean isEmpty() {
-        return mBag.length == 0;
+        return mElements.length == 0;
     }
 
     @Override
     public boolean add(T newEntry) {
-        if (mCurrentElementIndex < mBag.length) {
-            mBag[mCurrentElementIndex] = newEntry;
+        if (mCurrentElementIndex < mElements.length) {
+            mElements[mCurrentElementIndex] = newEntry;
             mCurrentElementIndex++;
             return true;
         }
@@ -38,16 +37,16 @@ public class Bag<T> implements IBag<T> {
 
     @Override
     public T remove() {
-        T currentElement = mBag[mCurrentElementIndex];
+        T currentElement = mElements[mCurrentElementIndex];
         mCurrentElementIndex--;
         return currentElement;
     }
 
     @Override
     public boolean remove(T anEntry) {
-        for (int i = 0; i < mBag.length; i++) {
-            if (mBag[i].equals(anEntry)) {
-                mBag[i] = mBag[mCurrentElementIndex];
+        for (int i = 0; i < mElements.length; i++) {
+            if (mElements[i].equals(anEntry)) {
+                mElements[i] = mElements[mCurrentElementIndex];
                 mCurrentElementIndex--;
                 return true;
             }
@@ -57,16 +56,16 @@ public class Bag<T> implements IBag<T> {
 
     @Override
     public void clear() {
-        for (int i = 0; i < mBag.length; i++) {
-            mBag[i] = null;
+        for (int i = 0; i < mElements.length; i++) {
+            mElements[i] = null;
         }
     }
 
     @Override
     public int getFrequencyOf(T anEntry) {
         int frequency = 0;
-        for (int i = 0; i < mBag.length; i++) {
-            if (mBag[i].equals(anEntry)) {
+        for (int i = 0; i < mElements.length; i++) {
+            if (mElements[i].equals(anEntry)) {
                 frequency++;
             }
         }
@@ -75,8 +74,8 @@ public class Bag<T> implements IBag<T> {
 
     @Override
     public boolean contains(T anEntry) {
-        for (int i = 0; i < mBag.length; i++) {
-            if (mBag[i].equals(anEntry)) {
+        for (int i = 0; i < mElements.length; i++) {
+            if (mElements[i].equals(anEntry)) {
                 return true;
             }
         }
@@ -87,8 +86,13 @@ public class Bag<T> implements IBag<T> {
     public T[] toArray() {
         T[] newElements = (T[]) new Object[mCurrentElementIndex];
         for (int i = 0; i < mCurrentElementIndex; i++) {
-            newElements[i] = mBag[i];
+            newElements[i] = mElements[i];
         }
         return newElements;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(mElements);
     }
 }
