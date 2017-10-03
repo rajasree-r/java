018a2f9401b09a9869680e57e37cbd60afa71db3
Rajasree <me.shree@gmail.com>
2017-10-02 21:51:43 -0700

    adding some more

---
+++ src/main/jithu/abstractjava/arrays/Sample.java
@@ -0,0 +1,7 @@
+package jithu.abstractjava.arrays;
+
+/**
+ * Created by aj on 10/2/17.
+ */
+public class Sample {
+}
---
+++ src/test/jithu/abstractjava/BagTest.java
@@ -0,0 +1,7 @@
+package jithu.abstractjava;
+
+/**
+ * Created by aj on 10/2/17.
+ */
+public class BagTest {
+}
