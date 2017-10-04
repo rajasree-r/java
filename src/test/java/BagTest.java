3050b470c7efd7a622a74bf2c7a545b1da3004b7
Rajasree <me.shree@gmail.com>
2017-10-03 22:25:41 -0700

    Initiating tests

---
+++ src/test/java/BagTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by aj on 10/3/17.
+ */
+public class BagTest {
+
+//    @ParameterizedTest
+//    @EnumSource(TimeUnit.class)
+//    void testWithExplicitArgumentConversion(@ConvertWith(ToStringArgumentConverter.class) String argument) {
+//        assertNotNull(TimeUnit.valueOf(argument));
+//    }
+
+    @Test
+    public void test() {
+        assertEquals(3, 1 + 2);
+    }
+}
